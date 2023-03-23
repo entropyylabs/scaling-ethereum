1462342dec6bbc3e912bf8d751406930b29c26f1
Ayush Jain <ayushjain2205@gmail.com>
2023-03-23 19:34:59 +0530

    Init base react native folder with thirdweb sdk configured

---
+++ tap-test/App.js
@@ -0,0 +1,56 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import { ConnectWallet, ThirdwebProvider } from "@thirdweb-dev/react-native";
+import React from "react";
+import {
+  SafeAreaView,
+  StyleSheet,
+  useColorScheme,
+  View,
+  Text,
+} from "react-native";
+import { Colors } from "react-native/Libraries/NewAppScreen";
+
+const App = () => {
+  return (
+    <ThirdwebProvider activeChain={"mumbai"}>
+      <AppInner />
+    </ThirdwebProvider>
+  );
+};
+
+const AppInner = () => {
+  const isDarkMode = useColorScheme() === "dark";
+  const backgroundStyle = {
+    backgroundColor: Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <View style={styles.view}>
+        <Text>Hello</Text>
+        <ConnectWallet />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  view: {
+    height: "100%",
+    display: "flex",
+    justifyContent: "space-evenly",
+    alignItems: "center",
+    alignContent: "center",
+  },
+});
+
+export default App;
